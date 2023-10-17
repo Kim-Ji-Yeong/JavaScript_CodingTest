c669a10d185aee5082c309bf7eb8785c1d9460af
Kim-Ji-Yeong <yeong6415@daum.net>
2023-10-17 16:44:08 +0900

    문자열 겹쳐쓰기

--- Level_0/22.js
+++ Level_0/22.js
@@ -1,3 +1,7 @@
-function solution(my_string, overwrite_string, s) {    
-    return my_string.substring(0,s)+overwrite_string+my_string.substring(s+overwrite_string.length)
+function solution(my_string, overwrite_string, s) {
+  return (
+    my_string.slice(0, s) +
+    overwrite_string +
+    my_string.slice(s + overwrite_string.length)
+  );
 }
